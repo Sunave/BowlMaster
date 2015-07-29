328dd71fdb1aaf29ecf57529115e09385b39f8fc
Sunave <antti.suniala@gmail.com>
2015-07-29 17:16:18 +0300

    Add upright pin detection

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+
+	public float standingThreshold = 3;
+
+	public bool IsStanding () {
+		return Vector3.Angle(Vector3.up, transform.up) < standingThreshold;
+	}
+}
