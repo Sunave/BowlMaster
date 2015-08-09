3d051a87392a43e143189fb849bf900220296055
Sunave <antti.suniala@gmail.com>
2015-08-09 14:18:58 +0300

    Add standing pin counter

---
+++ Assets/Scripts/PinCountUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PinCountUI : MonoBehaviour {
+
+	private Text text;
+	private PinSetter setter;
+	
+	void Start () {
+		text = GetComponent<Text>();
+		setter = GameObject.FindObjectOfType<PinSetter>();
+	}
+
+	void Update () {
+		text.text = setter.CountStanding().ToString();
+	}
+}
---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinSetter : MonoBehaviour {
+
+	private Pin[] pins;
+	
+	void Start () {
+		pins = GameObject.FindObjectsOfType<Pin>();
+	}
+
+	void Update () {
+		print (CountStanding());
+	}
+
+	public int CountStanding() {
+		int count = 0;
+		foreach (Pin pin in pins) {
+			if (pin.IsStanding()) count++;
+		}
+		return count;
+	}
+}
