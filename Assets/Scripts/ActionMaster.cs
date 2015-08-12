6f58c1316f93719b0f3e479c9d0b25edd3c098aa
Sunave <antti.suniala@gmail.com>
2015-08-12 16:15:34 +0300

    Add Unity Test Tools, ActionMaster class and first test for ActionMaster

---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMaster {
+
+	public enum NextAction {Tidy, Reset, EndTurn, EndGame};
+
+	public NextAction Bowl (int pins) {
+		if (pins < 0 || pins > 10) throw new UnityException ("Invalid pin count.");
+
+		if (pins == 10) return NextAction.EndTurn;
+
+		throw new UnityException ("Not sure which action to return");
+	}
+}
---
+++ Assets/Tests/Editor/ActionMasterTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+
+public class ActionMasterTest {
+	private ActionMaster.NextAction endTurn = ActionMaster.NextAction.EndTurn;
+
+	[Test]
+	public void T00PassingTest () {
+		Assert.AreEqual (1, 1);
+	}
+
+	[Test]
+	public void T01StrikeOnFirstReturnsEndOfTurn () {
+		ActionMaster actionMaster = new ActionMaster();
+		Assert.AreEqual (endTurn, actionMaster.Bowl(10));
+	}
+}
