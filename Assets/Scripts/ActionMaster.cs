87f084a2d67c6eb7f1737891d72227340e1e8753
Sunave <antti.suniala@gmail.com>
2015-08-13 14:48:40 +0300

    Add all ActionMaster logic and tests

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -5,11 +5,41 @@ public class ActionMaster {
 
 	public enum NextAction {Tidy, Reset, EndTurn, EndGame};
 
+	private int[] bowls = new int[21];
+	private int bowl = 1;
+
 	public NextAction Bowl (int pins) {
 		if (pins < 0 || pins > 10) throw new UnityException ("Invalid pin count.");
 
-		if (pins == 10) return NextAction.EndTurn;
+		bowls [bowl - 1] = pins;
+
+		if (bowl == 21) return NextAction.EndGame;
+
+		if (bowl >= 19 && Bowl21Awarded()) {
+			bowl++;
+			if (bowls[19-1] == 10 && pins < 10) return NextAction.Tidy;
+			else return NextAction.Reset;
+		} else if (bowl == 20 && !Bowl21Awarded()) {
+			return NextAction.EndGame;
+		}
+
+		if (pins == 10) {
+			bowl += 2;
+			return NextAction.EndTurn;
+		}
+
+		if (bowl % 2 != 0) { // Mid frame, or last frame
+			bowl++;
+			return NextAction.Tidy;
+		} else if (bowl % 2 == 0) { // End of frame
+			bowl++;
+			return NextAction.EndTurn;
+		}
 
 		throw new UnityException ("Not sure which action to return");
+	}
+
+	private bool Bowl21Awarded () {
+		return (bowls[19 - 1] + bowls[20 - 1]) >= 10;
 	}
 }
--- Assets/Tests/Editor/ActionMasterTest.cs
+++ Assets/Tests/Editor/ActionMasterTest.cs
@@ -6,7 +6,16 @@ using UnityEngine;
 [TestFixture]
 
 public class ActionMasterTest {
+	private ActionMaster actionMaster;
 	private ActionMaster.NextAction endTurn = ActionMaster.NextAction.EndTurn;
+	private ActionMaster.NextAction tidy = ActionMaster.NextAction.Tidy;
+	private ActionMaster.NextAction reset = ActionMaster.NextAction.Reset;
+	private ActionMaster.NextAction endGame = ActionMaster.NextAction.EndGame;
+
+	[SetUp]
+	public void SetupActionMaster () {
+		actionMaster = new ActionMaster();
+	}
 
 	[Test]
 	public void T00PassingTest () {
@@ -15,7 +24,80 @@ public class ActionMasterTest {
 
 	[Test]
 	public void T01StrikeOnFirstReturnsEndOfTurn () {
-		ActionMaster actionMaster = new ActionMaster();
 		Assert.AreEqual (endTurn, actionMaster.Bowl(10));
+	}
+
+	[Test]
+	public void T02Bowl8ReturnsTidy () {
+		Assert.AreEqual (tidy, actionMaster.Bowl(8));
+	}
+
+	[Test]
+	public void T03TwoBowlsReturnEndTurn () {
+		actionMaster.Bowl(8);
+		Assert.AreEqual (endTurn, actionMaster.Bowl(1));
+	}
+
+	[Test]
+	public void T04TwoBowlSpareReturnsEndTurn () {
+		actionMaster.Bowl (8);
+		Assert.AreEqual (endTurn, actionMaster.Bowl(2));
+	}
+
+	[Test]
+	public void T05After21BowlsReturnsEndGame () {
+		RollMany(2, 20);
+		Assert.AreEqual (endGame, actionMaster.Bowl(2));
+	}
+
+	[Test]
+	public void T06ResetsAtStrikeInLastFrame () {
+		RollMany(1, 18);
+		Assert.AreEqual (reset, actionMaster.Bowl (10));
+	}
+
+	[Test]
+	public void T07ResetsAtSpareInLastFrame () {
+		RollMany(1, 18);
+		actionMaster.Bowl (1);
+		Assert.AreEqual (reset, actionMaster.Bowl (9));
+	}
+
+	[Test]
+	public void T08YoutubeReferenceRollsEndGame () {
+		// Tutorial rolls from https://www.youtube.com/watch?v=aBe71sD8o8c
+		int[] rolls = {8,2, 7,3, 3,4, 10, 2,8, 10, 10, 8,0, 10, 8,2};
+		foreach (int roll in rolls) {
+			actionMaster.Bowl (roll);
+		}
+		Assert.AreEqual (endGame, actionMaster.Bowl (9));
+	}
+
+	[Test]
+	public void T09GameEndsAtBowl20 () {
+		RollMany(1, 19);
+		Assert.AreEqual (endGame, actionMaster.Bowl (1));
+	}
+
+	[Test]
+	public void T10Bowl20IsTidyAfterStrike () {
+		RollMany(1, 18);
+		actionMaster.Bowl(10);
+		Assert.AreEqual (tidy, actionMaster.Bowl(5));
+	}
+
+	[Test]
+	public void T11Bowl20IsTidyEvenIfZeroAfterStrike () {
+		RollMany(1, 18);
+		actionMaster.Bowl(10);
+		Assert.AreEqual (tidy, actionMaster.Bowl(0));
+	}
+
+
+
+	private void RollMany(int pinAmount, int amountOfRolls) {
+		for (int i = 0; i < amountOfRolls; i++) {
+			actionMaster.Bowl (pinAmount);
+		}
 	}
 }
